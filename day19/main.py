1c2ce581fed2d2820b2ad286d4def135c3c13419
theandrew168 <andrew@shallowbrooksoftware.com>
2021-12-19 03:18:20 -0600

    Bit more progress, time for bed

--- day19/main.py
+++ day19/main.py
@@ -1,5 +1,6 @@
 from collections import Counter, namedtuple
 import fileinput
+import itertools
 import math
 
 Point = namedtuple('Point', 'x y z')
@@ -8,19 +9,6 @@ Point = namedtuple('Point', 'x y z')
 # direction and orientation is random (xyz * up -> 6 * 4 = 24)
 # how do the points relate independent of view?
 # distances between? use that to trim the search space?
-
-
-def num_conns(n):
-    return (n * (n - 1)) / 2
-
-
-def num_nodes(c):
-    for n in range(100):
-        cs = num_conns(n)
-        if cs == c:
-            return n
-
-    return None
 
 
 def add(a, b):
@@ -45,78 +33,60 @@ def dist(a, b):
     return math.sqrt(d)
 
 
-def all_dists(points):
-    points = points.copy()
+def dist_combos(a_points, b_points):
+    alen = len(a_points)
+    blen = len(b_points)
 
-    dists = {}
-    while points[1:]:
-        pa = points[0]
-        for pb in points[1:]:
-            d = dist(pa, pb)
-            dists[(pa, pb)] = d
-        points = points[1:]
+    dists = Counter()
+    lim = max(alen, blen)
+    for a, b in itertools.combinations(range(lim), 2):
+        if a >= alen:
+            continue
+        if b >= blen:
+            continue
+
+        s = dist(a_points[a], b_points[b])
+        dists[s] += 1
 
     return dists
 
 
-def pair_with_dist(dists, dist):
-    for pair, d in dists.items():
-        if d == dist:
-            return pair
-
-
-# determine how to make the other points match the base
-def align(base, others):
-    xforms = [
-        lambda p: Point(p.x, p.y, p.z),
+def orientations(points):
+    flips = [
+        lambda p: Point( p.x,  p.y,  p.z),
+        lambda p: Point( p.x,  p.y, -p.z),
+        lambda p: Point( p.x, -p.y,  p.z),
+        lambda p: Point( p.x, -p.y, -p.z),
+        lambda p: Point(-p.x,  p.y,  p.z),
+        lambda p: Point(-p.x,  p.y, -p.z),
+        lambda p: Point(-p.x, -p.y,  p.z),
+        lambda p: Point(-p.x, -p.y, -p.z),
     ]
 
-    print(others)
-    for xform in xforms:
-        maybe = set(map(xform, others))
-        print(base, maybe)
-        print(base == maybe)
+    swaps = [
+        lambda p: Point(p.x, p.y, p.z),
+        lambda p: Point(p.x, p.z, p.y),
+        lambda p: Point(p.y, p.x, p.z),
+        lambda p: Point(p.y, p.z, p.x),
+        lambda p: Point(p.z, p.x, p.y),
+        lambda p: Point(p.y, p.z, p.x),
+    ]
+
+    for flip in flips:
+        for swap in swaps:
+            yield list(map(flip, map(swap, points)))
 
 
 def part1(scanners):
-    scanner_dists = []
-    for i, points in enumerate(scanners):
-        dists = all_dists(points)
-        scanner_dists.append(dists)
+    for a, b in itertools.combinations(range(len(scanners)), 2):
+        print(a, 'o', b)
+        ascan = scanners[a]
+        bscan = scanners[b]
+        for o in orientations(bscan):
+            dists = dist_combos(ascan, o)
+            print(dists.most_common(2))
 
-    total = sum(len(s) for s in scanners)
-
-    idx = 0
-    while scanner_dists[1:]:
-        this = scanner_dists[0]
-        for i, that in enumerate(scanner_dists[1:], start=1):
-            sames = set(this.values()) & set(that.values())
-            nodes = num_nodes(len(sames))
-            total -= nodes
-            if len(sames) >= num_conns(12):
-                print(idx, 'overlaps', i + idx)
-
-                pairs = []
-                this_pts = set()
-                for dist in sames:
-                    pair = pair_with_dist(this, dist)
-                    this_pts.add(pair[0])
-                    this_pts.add(pair[1])
-
-                that_pts = set()
-                for dist in sames:
-                    pair = pair_with_dist(that, dist)
-                    that_pts.add(pair[0])
-                    that_pts.add(pair[1])
-
-                print(len(this_pts), this_pts)
-                print(len(that_pts), that_pts)
-                #align(this_pts, that_pts)
-
-        scanner_dists = scanner_dists[1:]
-        idx += 1
-
-    print(total)
+        break
 
 
 def part2(scanners):
@@ -146,5 +116,39 @@ if __name__ == '__main__':
         point = Point(x, y, z)
         points.append(point)
 
-    print(part1(scanners))
-    print(part2(scanners))
+    zeroes = [
+        Point(-618,-824,-621),
+        Point(-537,-823,-458),
+        Point(-447,-329,318),
+        Point(404,-588,-901),
+        Point(544,-627,-890),
+        Point(528,-643,409),
+        Point(-661,-816,-575),
+        Point(390,-675,-793),
+        Point(423,-701,434),
+        Point(-345,-311,381),
+        Point(459,-707,401),
+        Point(-485,-357,347),
+    ]
+
+    ones = [
+        Point(686,422,578),
+        Point(605,423,415),
+        Point(515,917,-361),
+        Point(-336,658,858),
+        Point(-476,619,847),
+        Point(-460,603,-452),
+        Point(729,430,532),
+        Point(-322,571,750),
+        Point(-355,545,-477),
+        Point(413,935,-424),
+        Point(-391,539,-444),
+        Point(553,889,-390),
+    ]
+
+    for oo in orientations(ones):
+        for a, b in zip(zeroes, oo):
+            print(dist(a, b))
+
+    print(part1(scanners.copy()))
+    print(part2(scanners.copy()))
