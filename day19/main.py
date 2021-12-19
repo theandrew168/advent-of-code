c8dc674c5d6c100ae2dc7348c16dc49d051c9db5
theandrew168 <andrew@shallowbrooksoftware.com>
2021-12-19 12:43:24 -0600

    Align all scanners (need to translate)

--- day19/main.py
+++ day19/main.py
@@ -1,14 +1,11 @@
 from collections import Counter, namedtuple
 import fileinput
+from functools import partial
 import itertools
 import math
 
 Point = namedtuple('Point', 'x y z')
-
-# scanner sees within 1000 units x, y, z
-# direction and orientation is random (xyz * up -> 6 * 4 = 24)
-# how do the points relate independent of view?
-# distances between? use that to trim the search space?
+Matrix = namedtuple('Matrix', 'r0 r1 r2')
 
 
 def add(a, b):
@@ -25,6 +22,10 @@ def sub(a, b):
     return Point(dx, dy, dz)
 
 
+def dot(a, b):
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
+
+
 def dist(a, b):
     dx = b.x - a.x
     dy = b.y - a.y
@@ -33,60 +34,171 @@ def dist(a, b):
     return math.sqrt(d)
 
 
-def dist_combos(a_points, b_points):
-    alen = len(a_points)
-    blen = len(b_points)
-
+def dist_dense(a_points, b_points):
     dists = Counter()
-    lim = max(alen, blen)
-    for a, b in itertools.combinations(range(lim), 2):
-        if a >= alen:
-            continue
-        if b >= blen:
-            continue
-
-        s = dist(a_points[a], b_points[b])
-        dists[s] += 1
+    for a, b in itertools.product(a_points, b_points):
+        d = dist(a, b)
+        dists[d] += 1
 
     return dists
 
 
+def matmul(matrix, point):
+    x, y, z = [dot(point, row) for row in matrix]
+    return Point(x, y, z)
+
+
 def orientations(points):
-    flips = [
-        lambda p: Point( p.x,  p.y,  p.z),
-        lambda p: Point( p.x,  p.y, -p.z),
-        lambda p: Point( p.x, -p.y,  p.z),
-        lambda p: Point( p.x, -p.y, -p.z),
-        lambda p: Point(-p.x,  p.y,  p.z),
-        lambda p: Point(-p.x,  p.y, -p.z),
-        lambda p: Point(-p.x, -p.y,  p.z),
-        lambda p: Point(-p.x, -p.y, -p.z),
+    # http://www.euclideanspace.com/maths/algebra/matrix/transforms/examples/index.htm
+    xforms = [
+        Matrix(
+            Point( 1,  0,  0),
+            Point( 0,  1,  0),
+            Point( 0,  0,  1),
+        ),
+        Matrix(
+            Point( 1,  0,  0),
+            Point( 0,  0, -1),
+            Point( 0,  1,  0),
+        ),
+        Matrix(
+            Point( 1,  0,  0),
+            Point( 0, -1,  0),
+            Point( 0,  0, -1),
+        ),
+        Matrix(
+            Point( 1,  0,  0),
+            Point( 0,  0,  1),
+            Point( 0, -1,  0),
+        ),
+
+        Matrix(
+            Point( 0, -1,  0),
+            Point( 1,  0,  0),
+            Point( 0,  0,  1),
+        ),
+        Matrix(
+            Point( 0,  0,  1),
+            Point( 1,  0,  0),
+            Point( 0,  1,  0),
+        ),
+        Matrix(
+            Point( 0,  1,  0),
+            Point( 1,  0,  0),
+            Point( 0,  0, -1),
+        ),
+        Matrix(
+            Point( 0,  0, -1),
+            Point( 1,  0,  0),
+            Point( 0, -1,  0),
+        ),
+
+        Matrix(
+            Point(-1,  0,  0),
+            Point( 0, -1,  0),
+            Point( 0,  0,  1),
+        ),
+        Matrix(
+            Point(-1,  0,  0),
+            Point( 0,  0, -1),
+            Point( 0, -1,  0),
+        ),
+        Matrix(
+            Point(-1,  0,  0),
+            Point( 0,  1,  0),
+            Point( 0,  0, -1),
+        ),
+        Matrix(
+            Point(-1,  0,  0),
+            Point( 0,  0,  1),
+            Point( 0,  1,  0),
+        ),
+
+        Matrix(
+            Point( 0,  1,  0),
+            Point(-1,  0,  0),
+            Point( 0,  0,  1),
+        ),
+        Matrix(
+            Point( 0,  0,  1),
+            Point(-1,  0,  0),
+            Point( 0, -1,  0),
+        ),
+        Matrix(
+            Point( 0, -1,  0),
+            Point(-1,  0,  0),
+            Point( 0,  0, -1),
+        ),
+        Matrix(
+            Point( 0,  0, -1),
+            Point(-1,  0,  0),
+            Point( 0,  1,  0),
+        ),
+
+        Matrix(
+            Point( 0,  0, -1),
+            Point( 0,  1,  0),
+            Point( 1,  0,  0),
+        ),
+        Matrix(
+            Point( 0,  1,  0),
+            Point( 0,  0,  1),
+            Point( 1,  0,  0),
+        ),
+        Matrix(
+            Point( 0,  0,  1),
+            Point( 0, -1,  0),
+            Point( 1,  0,  0),
+        ),
+        Matrix(
+            Point( 0, -1,  0),
+            Point( 0,  0, -1),
+            Point( 1,  0,  0),
+        ),
+
+        Matrix(
+            Point( 0,  0, -1),
+            Point( 0, -1,  0),
+            Point(-1,  0,  0),
+        ),
+        Matrix(
+            Point( 0, -1,  0),
+            Point( 0,  0,  1),
+            Point(-1,  0,  0),
+        ),
+        Matrix(
+            Point( 0,  0,  1),
+            Point( 0,  1,  0),
+            Point(-1,  0,  0),
+        ),
+        Matrix(
+            Point( 0,  1,  0),
+            Point( 0,  0, -1),
+            Point(-1,  0,  0),
+        ),
     ]
 
-    swaps = [
-        lambda p: Point(p.x, p.y, p.z),
-        lambda p: Point(p.x, p.z, p.y),
-        lambda p: Point(p.y, p.x, p.z),
-        lambda p: Point(p.y, p.z, p.x),
-        lambda p: Point(p.z, p.x, p.y),
-        lambda p: Point(p.y, p.z, p.x),
-    ]
-
-    for flip in flips:
-        for swap in swaps:
-            yield list(map(flip, map(swap, points)))
+    for xform in xforms:
+        xform_func = partial(matmul, xform)
+        yield tuple(map(xform_func, points))
 
 
 def part1(scanners):
-    for a, b in itertools.combinations(range(len(scanners)), 2):
-        print(a, 'o', b)
-        ascan = scanners[a]
-        bscan = scanners[b]
-        for o in orientations(bscan):
-            dists = dist_combos(ascan, o)
-            print(dists.most_common(2))
+    based, others = set(scanners[:1]), set(scanners[1:])
 
-        break
+    # align each other scanner for the first one
+    while others:
+        try:
+            for base, other in itertools.product(based.copy(), others.copy()):
+                for o in orientations(other):
+                    dists = dist_dense(base, o)
+                    if any(c >= 12 for c in dists.values()):
+                        print('found an alignment!', len(based))
+                        based.add(o)
+                        others.remove(other)
+                        raise StopIteration
+        except StopIteration:
+            pass
 
 
 def part2(scanners):
@@ -107,7 +219,7 @@ if __name__ == '__main__':
 
         # check for end of scanner points
         if len(line) == 0:
-            scanners.append(points)
+            scanners.append(tuple(points))
             continue
 
         x, y, z = line.split(',')
@@ -115,40 +227,6 @@ if __name__ == '__main__':
 
         point = Point(x, y, z)
         points.append(point)
-
-    zeroes = [
-        Point(-618,-824,-621),
-        Point(-537,-823,-458),
-        Point(-447,-329,318),
-        Point(404,-588,-901),
-        Point(544,-627,-890),
-        Point(528,-643,409),
-        Point(-661,-816,-575),
-        Point(390,-675,-793),
-        Point(423,-701,434),
-        Point(-345,-311,381),
-        Point(459,-707,401),
-        Point(-485,-357,347),
-    ]
-
-    ones = [
-        Point(686,422,578),
-        Point(605,423,415),
-        Point(515,917,-361),
-        Point(-336,658,858),
-        Point(-476,619,847),
-        Point(-460,603,-452),
-        Point(729,430,532),
-        Point(-322,571,750),
-        Point(-355,545,-477),
-        Point(413,935,-424),
-        Point(-391,539,-444),
-        Point(553,889,-390),
-    ]
-
-    for oo in orientations(ones):
-        for a, b in zip(zeroes, oo):
-            print(dist(a, b))
 
     print(part1(scanners.copy()))
     print(part2(scanners.copy()))
