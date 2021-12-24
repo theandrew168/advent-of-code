739ce13db2aee3b6c7610e61752cc8bb52d32005
theandrew168 <andrew@shallowbrooksoftware.com>
2021-12-24 11:28:58 -0600

    Build a graph of instructions and registers

--- day24/main.py
+++ day24/main.py
@@ -1,69 +1,44 @@
 import fileinput
 
 
-class ALU:
+class Inst:
 
-    def __init__(self, data):
-        self.data = data
-        self.w = 0
-        self.x = 0
-        self.y = 0
-        self.z = 0
+    def __init__(self, name, dst, src):
+        self.name = name
+        self.dst = dst
+        self.src = src
 
     def __str__(self):
-        s = 'w={} x={} y={} z={}'
-        return s.format(self.w, self.x, self.y, self.z)
+        return self.name
 
-    def value(self, v):
-        try:
-            v = int(v)
-        except ValueError:
-            v = getattr(self, v)
-        return v
-
-    def execute(self, line):
-        inst, *args = line.split()
-        if len(args) == 2:
-            a, b = args
-        else:
-            a = args[0]
-            b = self.data[0]
-            self.data = self.data[1:]
-
-        if inst == 'inp':
-            setattr(self, a, int(b))
-        elif inst == 'add':
-            setattr(self, a, getattr(self, a) + self.value(b))
-        elif inst == 'mul':
-            setattr(self, a, getattr(self, a) * self.value(b))
-        elif inst == 'div':
-            setattr(self, a, getattr(self, a) // self.value(b))
-        elif inst == 'mod':
-            setattr(self, a, getattr(self, a) % self.value(b))
-        elif inst == 'eql':
-            setattr(self, a, 1 if getattr(self, a) == self.value(b) else 0)
-        else:
-            raise ValueError('invalid instruction: {}'.format(inst))
+    def __repr__(self):
+        return 'Inst({!r}, {!r}, {!r})'.format(self.name, self.dst, self.src)
 
 
-# cant brute force this
-# need to trace the formula back and then SAT solve?
-# maybe use pycosat or something
-# like that one minecraft redstone CTF from LiveOverflow
+# build an AST and do some analysis
 def part1(lines):
-    code = 99999999999999
-    while True:
-        print(code)
-        alu = ALU(str(code))
-        for line in lines:
-            alu.execute(line)
+    regs = [None] * 4
+    for line in reversed(lines):
+        name, dst, *src = line.split()
+        if len(src) == 1:
+            src = src[0]
+        else:
+            src = 'i'
 
-        if alu.z == 0:
-            break
+        inst = Inst(name, dst, src)
 
-        code -= 1
+        # build a graph of instructions
+        idx = 'wxyz'.index(dst)
+        inst.dst = regs[idx]
+        if inst.src in 'wxyz':
+            idx2 = 'wxyz'.index(inst.src)
+            inst.src = regs[idx2]
+        regs[idx] = inst
 
-    return code
+    for r in regs:
+        print(repr(r))
+
+    return 42
 
 
 def part2(lines):
