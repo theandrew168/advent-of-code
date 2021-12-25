8ebd76e30f93f5d0f2f04f32557c5bb9d9c73085
theandrew168 <andrew@shallowbrooksoftware.com>
2021-12-25 03:36:55 -0600

    Try a different strat, no luck

--- 2021/day24/main.py
+++ 2021/day24/main.py
@@ -45,60 +45,6 @@ class ALU:
             raise ValueError('invalid instruction: {}'.format(inst))
 
 
-# x = !(z % 26 + v2 == input)
-# z = (z / v1)      (1 or 26)
-# y = 25 * x + 1
-# z = z * y
-# y = (input + v3) * x
-# z = z + y
-
-# if z % 26 + v2 != input:
-#   z = z // v1 (1 or 26)
-#   z = z * 26
-#   z = z + input + v3
-# else:
-#   z = z // v1
-
-# if z & 1 + v2 != input:
-#   z = z >> v1 (0 or 1)
-#   z = z << 1
-#   z = z + input + v3
-# else:
-#   z = z >> v1 (0 or 1)
-
-# if last bit of z + v2 equal input:
-#   if v1, pop a bit
-# else:
-#   if v1, pop a bit
-#   push a bit
-#   z equal z plus input plus v3
-
-# if v1 == 0:
-#   if z.last + v2 != input:
-#       push a bit
-# elif v1 == 1:
-#   if z.last + v2 != input:
-#       pop a bit
-#       push a bit
-#   else:
-#       pop a bit
-
-# if v1 == 0:
-#   push a bit
-# elif v1 == 1:
-#   if z.last + v2 != input:
-#       pop a bit
-#       push a bit
-#   else:
-#       pop a bit
-
-# if v1 == 0:
-#   push a bit (input + v3)
-# elif v1 == 1:
-#   pop a bit
-#   if z.last + v2 != input:
-#       push a bit (input + v3)
-
 def part1(lines):
     progs = []
     for line in lines:
@@ -107,29 +53,43 @@ def part1(lines):
         else:
             progs[-1].append(line)
 
-    # (1, 10, 1)     push
-    # (1, 11, 9)     push
-    # (1, 14, 12)    push
-    # (1, 13, 6)     push
-    # (26, -6, 9)    pop
-    # (26, -14, 15)  pop
-    # (1, 14, 7)     push
-    # (1, 13, 12)    push
-    # (26, -8, 15)   pop
-    # (26, -15, 3)   pop
-    # (1, 10, 6)     push
-    # (26, -11, 2)   pop
-    # (26, -13, 10)  pop
-    # (26, -4, 12)   pop
+    want = [set([0])]
+    for prog in reversed(progs):
+        print('solving', len(want) - 1)
 
-    vars = []
-    for prog in progs:
-        v1 = int(prog[4].split()[-1])
-        v2 = int(prog[5].split()[-1])
-        v3 = int(prog[15].split()[-1])
-        vars.append((v1, v2, v3))
+        outputs = set()
+        for z in range(100000):
+            for c in range(1, 10):
+                alu = ALU(c)
+                alu.z = z
+                for inst in prog:
+                    alu.execute(inst)
 
-    return 42
+                if alu.z in want[-1]:
+                    outputs.add(z)
+                    break
+
+        if not outputs:
+            raise ValueError('no outputs')
+
+        want.append(outputs)
+
+    code = []
+    want = list(reversed(want))
+    for i, prog in enumerate(prog):
+        print('coding', len(code))
+        for z in want[i]:
+            for c in range(9, 0, -1):
+                alu = ALU(c)
+                alu.z = z
+                for inst in prog:
+                    alu.execute(prog)
+
+                if alu.z in want[i]:
+                    code.append(c)
+                    break
+
+    return ''.join(str(c) for c in code)
 
 
 def part2(lines):
