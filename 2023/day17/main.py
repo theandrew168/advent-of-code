f02cfa129c2cc59fa7ad1b5a02d67ba5076c9718
theandrew168 <andrew@shallowbrooksoftware.com>
2023-12-18 02:37:36 -0600

    2023 Day 17 Part 2

--- 2023/day17/main.py
+++ 2023/day17/main.py
@@ -1,9 +1,19 @@
 import fileinput
 from queue import PriorityQueue
 
-# < 1052
-# < 992
-# > 768
+OPP = {
+    'N': 'S',
+    'S': 'N',
+    'E': 'W',
+    'W': 'E',
+}
+
+OFF = {
+    'N': ( 0, -1),
+    'S': ( 0,  1),
+    'E': ( 1,  0),
+    'W': (-1,  0),
+}
 
 
 class Grid:
@@ -27,6 +37,7 @@ class Grid:
             yield x, yy, d
 
 
+# unused, just for research
 def shortest_path(grid, start, end):
     costs = {}
     costs[start] = 0
@@ -56,14 +67,6 @@ def shortest_path(grid, start, end):
     path.append(start)
     path.reverse()
     return path
-
-
-OPP = {
-    'N': 'S',
-    'S': 'N',
-    'E': 'W',
-    'W': 'E',
-}
 
 
 def part1(lines):
@@ -109,7 +112,65 @@ def part1(lines):
 
 
 def part2(lines):
-    pass
+    grid = Grid(lines)
+
+    start = (0, 0)
+    end = (grid.width-1, grid.height-1)
+
+    # nodes are: x, y, d, n
+
+    costs = {}
+    costs[(0, 0, None, None)] = 0
+
+    pq = PriorityQueue()
+    pq.put((0, (0, 0, None, None)))
+
+    # iterate until all nodes have been visited
+    while not pq.empty():
+        cost, curr = pq.get()
+
+        for x, y, d in grid.adj(curr[0], curr[1]):
+            if curr[2] == OPP[d]:
+                continue
+
+            n = curr[3] + 1 if d == curr[2] else 1
+            if n >= 11:
+                continue
+
+
+            adj = (x, y, d, n)
+            new_cost = cost + grid.get(x, y)
+
+            # if not at 4 steps, jump to the spot and push
+            if n < 4:
+                off = OFF[d]
+                px, py = x, y
+                viable = True
+                for _ in range(4 - n):
+                    mpx, mpy = px + off[0], py + off[1]
+                    if mpx < 0 or mpx >= grid.width or mpy < 0 or mpy >= grid.height:
+                        viable = False
+                        break
+                    px, py = mpx, mpy
+                    new_cost += grid.get(px, py)
+
+                if not viable:
+                    continue
+
+                adj = (px, py, d, 4)
+
+            if adj not in costs or new_cost < costs[adj]:
+                costs[adj] = new_cost
+                pq.put((new_cost, adj))
+
+    # lookup the best (lowest) cost
+    best = None
+    for k, cost in costs.items():
+        if k[0] == end[0] and k[1] == end[1]:
+            if best is None or cost < best:
+                best = cost
+
+    return best
 
 
 if __name__ == '__main__':
