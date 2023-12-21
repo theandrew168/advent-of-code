a79ad677cf203d6a060d9e9f154a83b19b752b9e
theandrew168 <andrew@shallowbrooksoftware.com>
2023-12-21 10:52:28 -0600

    2023 Day 21 Part 2

--- 2023/day21/main.py
+++ 2023/day21/main.py
@@ -24,6 +24,21 @@ class Grid:
                 continue
             yield xx, y
 
+    def get2(self, pt):
+        x, y = pt
+        x = x % self.w
+        y = y % self.h
+        return self.lines[y][x]
+
+    def adj2(self, pt):
+        x, y = pt
+        for yoff in [-1, 1]:
+            yy = y + yoff
+            yield x, yy
+        for xoff in [-1, 1]:
+            xx = x + xoff
+            yield xx, y
+
 
 def find_start(grid):
     for y in range(grid.h):
@@ -32,6 +47,14 @@ def find_start(grid):
             if grid.get(pt) == 'S':
                 return pt
     assert False
+
+
+def expand(lines, by=1):
+    new_lines = []
+    for _ in range(3):
+        for line in lines:
+            new_lines.append(line * 3)
+    return new_lines
 
 
 def part1(lines):
@@ -53,8 +76,61 @@ def part1(lines):
     return len(curr)
 
 
+# 1x1 map
+# curr 7423 129
+# curr 7434 130 <- stabilizes
+# curr 7427 131 <- edge
+# curr 7434 132
+# curr 7427 133
+
+# 3x3 map
+# curr 66878 654
+# curr 66871 655 <- stabilizes
+# curr 66878 656
+# curr 66871 657
+# curr 66878 658
+# curr 66871 659
+
+
+# samples at: (n-65) % 131 == 0
+# 65,3791
+# 196,33646
+# 327,93223
+# 458,182522
+
+# wolfram yields formula:
+# 14861n^2 - 14728n + 3658
+def quad(n):
+    n = (n-65) / 131
+    n += 1
+    return int(14861*n*n - 14728*n + 3658)
+
+
 def part2(lines):
-    pass
+    grid = Grid(lines)
+    start = find_start(grid)
+
+#    for n in [65,196,327,458,26501365]:
+#        print(quad(n))
+    return quad(26501365)
+
+    # used to generate a sample series
+    curr = set()
+    curr.add(start)
+    for i in range(1000):
+        nxt = set()
+        for pt in curr:
+            for adj in grid.adj2(pt):
+                c = grid.get2(adj)
+                if c == '#':
+                    continue
+                nxt.add(adj)
+        curr = nxt
+        step = i+1
+        if (step-65) % 131 == 0: 
+            print('{},{}'.format(step, len(curr)))
+
+    return len(curr)
 
 
 if __name__ == '__main__':
