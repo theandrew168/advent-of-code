88f80e03f754bf84630426b6c995c99131a04bd5
theandrew168 <andrew@shallowbrooksoftware.com>
2023-12-24 01:36:18 -0600

    2023 Day 24 Part 1

--- 2023/day24/main.py
+++ 2023/day24/main.py
@@ -1,4 +1,43 @@
+from collections import namedtuple
 import fileinput
+import itertools
+
+# < 19447
+
+
+Vector = namedtuple('Vector', 'x y z')
+
+
+class Ray:
+    def __init__(self, pos, vel):
+        self.pos = pos
+        self.vel = vel
+
+    def __str__(self):
+        s = '{}, {} @ {}, {}'
+        return s.format(*self.pos, *self.vel)
+
+    def at(self, t):
+        x = self.pos.x + self.vel.x * t
+        y = self.pos.y + self.vel.y * t
+        return (x, y)
+
+    def intersection(self, other):
+        az = self.pos
+        ad = self.vel
+        bs = other.pos
+        bd = other.vel
+
+        dx = bs.x - az.x
+        dy = bs.y - az.y
+        det = bd.x * ad.y - bd.y * ad.x
+        if det == 0:
+            return None
+        u = (dy * bd.x - dx * bd.y) / det
+        v = (dy * ad.x - dx * ad.y) / det
+        if u < 0 or v < 0:
+            return None
+        return u, v
 
 
 def parse(lines):
@@ -6,14 +45,32 @@ def parse(lines):
         pos, vel = line.split('@')
         pos = pos.strip()
         vel = vel.strip()
-        pos = tuple(int(n) for n in pos.split(', '))
-        vel = tuple(int(n) for n in vel.split(', '))
-        yield pos, vel
+        pos = tuple(float(n) for n in pos.split(', '))
+        vel = tuple(float(n) for n in vel.split(', '))
+        yield Vector(*pos), Vector(*vel)
+
+
+def solve2D(rays, start, end):
+    total = 0
+    for a, b in itertools.combinations(rays, 2):
+        inter = a.intersection(b)
+        if inter is None:
+            continue
+
+        at = a.at(inter[0])
+        if at[0] < start or at[0] > end:
+            continue
+        if at[1] < start or at[1] > end:
+            continue
+
+        total += 1
+
+    return total
 
 
 def part1(lines):
-    for p,v in parse(lines):
-        print(p,v)
+    rays = [Ray(pos, rel) for pos, rel in parse(lines)]
+    return solve2D(rays, 200_000_000_000_000, 400_000_000_000_000)
 
 
 def part2(lines):
