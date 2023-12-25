6630093b2f328dc8b4de7161df8287f5bd214cce
theandrew168 <andrew@shallowbrooksoftware.com>
2023-12-24 18:37:51 -0600

    2023 Day 24 Part 2

--- 2023/day24/main.py
+++ 2023/day24/main.py
@@ -68,10 +68,54 @@ def solve1(rays, start, end):
     return total
 
 
-def solve2(rays, start=-10, end=10, tm=20):
-    x = z3.Int('x')
-    y = z3.Int('y')
-    return z3.solve(x > 2, y < 10, x + 2*y == 7)
+# 44446443386018, 281342848485672, 166638492241385 @ 197, 16, 200
+# 119566840879742, 430566433235378, 268387686114969 @ 18, -130, 74
+# 433973471892198, 260061119249300, 263051300077633 @ -16, -170, -118
+def solve2():
+    x, y, z, dx, dy, dz = z3.Ints('x y z dx dy dz')
+    x0, y0, z0, dx0, dy0, dz0, t0 = z3.Ints('x0 y0 z0 dx0 dy0 dz0 t0')
+    x1, y1, z1, dx1, dy1, dz1, t1 = z3.Ints('x1 y1 z1 dx1 dy1 dz1 t1')
+    x2, y2, z2, dx2, dy2, dz2, t2 = z3.Ints('x2 y2 z2 dx2 dy2 dz2 t2')
+
+    equations = [
+        x + dx * t0 == x0 + dx0 * t0,
+        y + dy * t0 == y0 + dy0 * t0,
+        z + dz * t0 == z0 + dz0 * t0,
+        x + dx * t1 == x1 + dx1 * t1,
+        y + dy * t1 == y1 + dy1 * t1,
+        z + dz * t1 == z1 + dz1 * t1,
+        x + dx * t2 == x2 + dx2 * t2,
+        y + dy * t2 == y2 + dy2 * t2,
+        z + dz * t2 == z2 + dz2 * t2,
+    ]
+
+    problem = [
+        x0 == 44446443386018,
+        y0 == 281342848485672,
+        z0 == 166638492241385,
+        dx0 == 197,
+        dy0 == 16,
+        dz0 == 200,
+        x1 == 119566840879742,
+        y1 == 430566433235378,
+        z1 == 268387686114969,
+        dx1 == 18,
+        dy1 == -130,
+        dz1 == 74,
+        x2 == 433973471892198,
+        y2 == 260061119249300,
+        z2 == 263051300077633,
+        dx2 == -16,
+        dy2 == -170,
+        dz2 == -118,
+    ]
+
+    s = z3.Solver()
+    s.add(equations + problem)
+    s.check()
+
+    m = s.model()
+    return m[x].as_long() + m[y].as_long() + m[z].as_long()
 
 
 def part1(lines):
@@ -80,8 +124,7 @@ def part1(lines):
 
 
 def part2(lines):
-    rays = [Ray(pos, rel) for pos, rel in parse(lines)]
-    return solve2(rays)
+    return solve2()
 
 
 if __name__ == '__main__':
