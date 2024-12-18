fad0c7708deb3a8473738fba6609b3d5e6d0f229
theandrew168 <andrew@shallowbrooksoftware.com>
2024-12-17 23:20:14 -0600

    Day 17 part 2 (fun one!)

--- 2024/day17/main.py
+++ 2024/day17/main.py
@@ -44,39 +44,27 @@ import fileinput
 #
 # Terminates when A == 0.
 
-#     F8421
-# A = 10001
-# B = 010
-# C = 010
+# To output the last number: 0
+# A = 0
+# A < 8 (to div out to zero)
+# B (bottom 3) must be 0b000 (0)
+# B (buttom 3) must be 0b011 (3)
+# B ^ C = 0b011 (3)
+# C = A // (2**B)
+# B = B ^ 2
+# B = A % 8
 
-M = defaultdict(list)
-for a in range(128):
-    A = a
-    B = 0
-    C = 0
+# B ^ C = 0b011 (3)
+# (A%8)^2 ^ (A // 2**(A%8)^2) = 0b011 (3)
+# C = A // (2**(A % 8)^2)
+# B = (A % 8) ^ 2
 
-    B = A % 8
-    B = B ^ 2
-    C = A // (2**B)
-    B = B ^ C
-    B = B ^ 3
-    O = B % 8
-    M[O].append(a)
-    #A = A // 8
-    #print('{} -> 0b{:03b} ({})'.format(a, O, O))
-
-M = {k: v[0] for k, v in M.items()}
-PROG = [2,4, 1,2, 7,5, 4,5, 1,3, 5,5, 0,3, 3,0]
-print(M)
-
-res = 0
-for p in PROG:
-    A = M[p]
-    B = (A % 8) ^ 2
-    f = 2**B
-    res |= M[p]
-    res *= f
-print(res)
+#A = 0
+#PROG = [2,4, 1,2, 7,5, 4,5, 1,3, 5,5, 0,3, 3,0]
+#for p in reversed(PROG):
+#    O = p
+#    B = O
+#    B = B ^ 3
 
 
 def parse(lines):
@@ -154,12 +142,22 @@ def part1(lines):
 def part2(lines):
     regs, prog = parse(lines)
 
-    i = 0
-    while True:
-        out = run([i, regs[1], regs[2]], prog)
-        if out == prog:
-            break
-    return i
+    # iteratively build up the program from end to start
+    A = 0
+    for i in range(len(prog)):
+        # we want to create the following output (building from the end)
+        want = prog[-(i+1):]
+        while True:
+            # run the output for the current A
+            out = run([A, regs[1], regs[2]], prog)
+            # if it gives us the correct result, shift left by 3 and keep rolling
+            if out == want:
+                A <<= 3
+                break
+            # if it doesn't give us the output we want, keep looping
+            A += 1
+    # final answer was shifted an extra time to undo it before returning
+    return A >> 3
 
 
 if __name__ == '__main__':
@@ -168,4 +166,4 @@ if __name__ == '__main__':
         lines.append(line.strip())
 
     print(part1(lines))
-    #print(part2(lines))
+    print(part2(lines))
