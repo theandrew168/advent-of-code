2439074c367067d9065acd1222c26a0b769144fd
theandrew168 <andrew@shallowbrooksoftware.com>
2024-12-20 01:14:38 -0600

    Day 20 part 2

--- 2024/day20/main.py
+++ 2024/day20/main.py
@@ -1,5 +1,6 @@
 from collections import defaultdict, deque
 import fileinput
+import functools
 
 
 def find_start_end(lines):
@@ -29,7 +30,7 @@ def adj4(pt):
 def bfs(lines, start, end):
     seen = set()
 
-    q = deque([(0, start)])
+    q = deque([[start]])
     while q:
         cost, curr = q.popleft()
         if curr in seen:
@@ -62,6 +63,31 @@ def apply_skip(lines, x, y):
     return out
 
 
+def follow(lines, start, end):
+    path = [start]
+
+    seen = set()
+    seen.add(start)
+
+    curr = start
+    while curr != end:
+        for adj in adj4(curr):
+            if adj in seen:
+                continue
+
+            x, y = adj
+            c = lines[y][x]
+            if c == '#':
+                continue
+
+            path.append(adj)
+            seen.add(adj)
+            curr = adj
+            break
+
+    return path
+
+
 def part1(lines):
     width = len(lines[0])
     height = len(lines)
@@ -82,8 +108,60 @@ def part1(lines):
     return sum(v for k, v in diffs.items() if k >= 100)
 
 
+@functools.cache
+def dist(a, b):
+    return abs(b[0]-a[0]) + abs(b[1]-a[1])
+
+
+def diamond(center, size):
+    for yoff in range(-size, size+1):
+        for xoff in range(-size, size+1):
+            x, y = center
+            pt = (x+xoff, y+yoff)
+            d = dist(center, pt)
+            # skips of size 0 and 1 don't matter
+            if d == 0 or d == 1:
+                continue
+            # return only points with the grid-dist diamond
+            if dist(center, pt) <= size:
+                yield pt
+
+
 def part2(lines):
-    pass
+    skip = 20
+    width = len(lines[0])
+    height = len(lines)
+    start, end = find_start_end(lines)
+    path = follow(lines, start, end)
+    pathset = set(path)
+
+    diffs = defaultdict(int)
+    for i, pt in enumerate(path):
+        # the score here is the index in the base path
+        here = i
+        for dpt in diamond(pt, skip):
+            # skip doesn't end on the path, continue
+            if dpt not in pathset:
+                continue
+
+            # the score there is also the index in the base path
+            there = path.index(dpt)
+
+            # skipping behind doesn't help, continue
+            if there < here:
+                continue
+
+            # this skip saves the time diff between here and there
+            diff = there - here - dist(pt, dpt)
+
+            # skip didn't add value, continue
+            if diff == 0:
+                continue
+
+            # found a valid skip! count it!
+            diffs[diff] += 1
+
+    return sum(v for k, v in diffs.items() if k >= 100)
 
 
 if __name__ == '__main__':
@@ -91,5 +169,5 @@ if __name__ == '__main__':
     for line in fileinput.input():
         lines.append(line.strip())
 
-    print(part1(lines))
+    #print(part1(lines))
     print(part2(lines))
